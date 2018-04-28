61390a5d262435afdf6f530abbc1affd4f9b866b
Luis Miguel Paz <luismpazv@gmail.com>
2018-04-28 09:33:08 -0500

    First commit

---
+++ src/model/Gramatica.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.HashMap;
+
+public class Gramatica {
+	
+	
+	
+	private HashMap<String, Variable> producciones;
+	
+
+}
---
+++ src/model/Variable.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.util.HashSet;
+
+public class Variable {
+	
+	
+	private HashSet<String> producciones;
+	
+	private String identificador;
+
+	public Variable(String identificador) {
+		this.producciones = new HashSet<>();
+		this.identificador = identificador;
+		
+	}
+
+	public HashSet<String> getProducciones() {
+		return producciones;
+	}
+
+	public void setProducciones(HashSet<String> producciones) {
+		this.producciones = producciones;
+	}
+
+	public String getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
+	
+	
+	
+	
+
+}
